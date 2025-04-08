ffc1f33d66b59c7da4ba03b6ea6691b9bcacb263
Osmolovsky K. <kirillosm09@gmail.com>
2025-04-08 17:58:33 +0300

    feat: add cli argument parsing

--- src/caching_proxy/proxy.py
+++ src/caching_proxy/proxy.py
@@ -3,8 +3,8 @@ from urllib.parse import urlparse, urlunparse
 import argparse
 
 routes = web.RouteTableDef()
-DEST = "5.253.41.41" # TODO: get that from cmd
-DEST_PORT = "3246"
+DEST = "" 
+DEST_PORT = ""
 
 async def get_back(url: str):
     # url is url of destination
@@ -25,8 +25,33 @@ async def forward(request):
 
     return await get_back(dest_url.geturl())
 
+def setup_values(args):
+    ### seems bad
+    global DEST
+    global DEST_PORT
+    ###
+    if args.clear_cache:
+        #TODO: add cache clearing
+        pass
+
+    if not args.origin:
+        raise ValueError
+    DEST_PORT = args.port
+    DEST = args.origin
+    print(f"DEST={DEST}, DEST_PORT={DEST_PORT}")
+
+
 def main():
-    #parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+                prog='caching-proxy',
+                description="CLI caching proxy server"
+            )
+    parser.add_argument("--origin", help="Origin url")
+    parser.add_argument("--port", help="Origin port", default="80")
+    parser.add_argument("--clear-cache", help="Clear local cache")
+    parser.set_defaults(func=setup_values)
+    args = parser.parse_args()
+    args.func(args)
 
     app = web.Application()
     app.add_routes(routes)
